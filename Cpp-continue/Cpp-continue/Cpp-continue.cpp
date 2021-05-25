da1d5358b861e3069f6a6e9abbb11eb0e89ed807
Nightywish <chunyu9655@hotmail.com>
2021-05-25 13:02:32 -0400

    more stuff

--- Cpp-continue/Cpp-continue/Cpp-continue.cpp
+++ Cpp-continue/Cpp-continue/Cpp-continue.cpp
@@ -6,17 +6,20 @@ using namespace std;
 
 int main()
 {
-
 	// game censor practice
 	string censor = " yo what the hell?";
 	censor.replace(censor.find("hell"), 4, "^.^!");
-		cout << censor << endl;
-	
+	cout << censor << endl;
+
+	string greeting =" hello there";
+	cout << greeting.find_first_of("!") << endl;
+		if (greeting.find_first_of("!") == -1) 
+			cout<< "Not found" << endl;
 
 
 
 
-
+		return 0;
 
 }
 
